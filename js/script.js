78bb4a0bd0c48d6eb5bd0df7a959b2f9e51d65a4
Federico <crudofederico@gmail.com>
2021-02-16 13:11:08 +0100

    update courses

--- js/script.js
+++ js/script.js
@@ -2,7 +2,49 @@
 var app = new Vue({
     el: '#app',
     data: {
-        menu:["Home","Pages","Courses","Features","Blog","Shop"]
+        menu:["Home","Pages","Courses","Features","Blog","Shop"],
+        stats:[ 
+            {number:"1.926",infonumber:"finished"},{number:"100%",infonumber:"satisfaction rate"},
+            {number:"3092+",infonumber:"enrolled learners"}, {number:"200",infonumber:"online instructors"},
+            ],
+        courses:[
+            {
+                price:"30.00",
+                title:"How to be SuccessFul:Create a Growth Mindset For Success",
+                students:50,
+                lessons:3,
+            },
+            {
+                price:"30.00",
+                title:"How to Build Confidence in Your Abilities",
+                students:50,
+                lessons:1,
+            },
+            {
+                price:"20.00",
+                title:"Productivity Machine-Focus in a Distracated World",
+                students:50,
+                lessons:5,
+            },
+            {
+                price:"20.00",
+                title:"Effective Time Management Mastery - Complete Guide",
+                students:50,
+                lessons:18,
+            },
+            {
+                price:"30.00",
+                title:"Body Language Secrets for Enterpreneurs",
+                students:50,
+                lessons:19,
+            },
+            {
+                price:"19.99",
+                title:"Management Skills:The Science of Leadership",
+                students:50,
+                lessons:17,
+            },
+        ]    
     },
     methods:{
        
